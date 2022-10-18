b6729cbdc4de294b153354d94668a5a9cb722dab
Andrey <81263467+GUN-SET@users.noreply.github.com>
2022-10-18 20:02:35 +0300

    Started developing an online movie theater using MongoDB & NextJS & Typescript & Nest.js

---
+++ server/src/auth/auth.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { UserModel } from '../user/user.model'
+import { JwtModule } from '@nestjs/jwt'
+import { getJwtConfig } from '../../config/jwt.config'
+import { JwtStrategy } from './strategy/jwt.strategy'
+
+@Module({
+	controllers: [AuthController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User'
+				}
+			}
+		]),
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	],
+
+	providers: [AuthService, JwtStrategy]
+})
+export class AuthModule {}
