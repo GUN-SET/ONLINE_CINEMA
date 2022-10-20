8283a9500c8c0db49657ef696ae0ca7abcb2d2c1
Andrey <81263467+GUN-SET@users.noreply.github.com>
2022-10-20 23:45:50 +0300

    Set up distribution of statics, file uploads, functionality for working with genres and actors

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,10 @@ import { TypegooseModule } from 'nestjs-typegoose'
 import { getMongoDbConfig } from '../config/mongo.config'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module';
+import { FileModule } from './file/file.module';
+import { ActorModule } from './actor/actor.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
 	imports: [
@@ -16,7 +20,11 @@ import { UserModule } from './user/user.module';
 			useFactory: getMongoDbConfig
 		}),
 		AuthModule,
-		UserModule
+		UserModule,
+		GenreModule,
+		FileModule,
+		ActorModule,
+		MovieModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
---
+++ server/src/genre/genre.service.ts
@@ -0,0 +1,92 @@
+import {Injectable, NotFoundException} from '@nestjs/common'
+import {InjectModel} from 'nestjs-typegoose'
+import {ModelType} from '@typegoose/typegoose/lib/types'
+import {GenreModel} from './genre.model'
+import {CreateGenreDto} from './dto/create-genre.dto'
+
+@Injectable()
+export class GenreService {
+	constructor(
+		@InjectModel(GenreModel) private readonly GenreModel: ModelType<GenreModel>
+	) {}
+
+	async bySlug(slug: string) {
+		const doc = await this.GenreModel.findOne({slug}).exec()
+		if (!doc) throw new NotFoundException('Genre not found')
+
+		return doc
+	}
+
+	async getAll(searchTerm?: string) {
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		let options: {}
+
+		if (searchTerm)
+			options = {
+				$or: [
+					{
+						name: new RegExp(searchTerm, 'i')
+					},
+					{
+						slug: new RegExp(searchTerm, 'i')
+					},
+					{
+						description: new RegExp(searchTerm, 'i')
+					}
+				]
+			}
+
+		return this.GenreModel.find(options)
+			.select('-updatedAt -__v')
+			.sort({
+				createdAt: 'desc'
+			})
+			.exec()
+	}
+
+	async getCollections() {
+		const genres = await this.getAll()
+		const collections = genres
+		/* NEED WRITE*/
+		return collections
+	}
+
+	/* ADMIN PLACE */
+	async byId(_id: string) {
+		const genre = await this.GenreModel.findById(_id)
+		if (!genre) throw new NotFoundException('Genre not found')
+
+		return genre
+	}
+
+	async create() {
+		const defaultValue: CreateGenreDto = {
+			name: '',
+			slug: '',
+			description: '',
+			icon: ''
+		}
+
+		const genre = await this.GenreModel.create(defaultValue)
+
+		return genre._id
+	}
+
+	async update(_id: string, dto: CreateGenreDto) {
+		const updateDoc = await this.GenreModel.findByIdAndUpdate(_id, dto, {
+			new: true
+		}).exec()
+
+		if (!updateDoc) throw new NotFoundException('Genre not found')
+
+		return updateDoc
+	}
+
+	async delete(_id: string) {
+		const deleteDoc = await this.GenreModel.findByIdAndDelete(_id).exec()
+
+		if (!deleteDoc) throw new NotFoundException('Genre not found')
+
+		return deleteDoc
+	}
+}
